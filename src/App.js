19823a8fd3b6aabf81b052b5bc31d98b23a1efd6
Coralie <crouveyrol@csumb.edu>
2023-10-25 06:42:52 +0200

    assignment6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import ListAssignment from './components/ListAssignment';
 import GradeAssignment from './components/GradeAssignment';
+import Login from './components/Login';
+import AddAssignment from './components/AddAssignment';
 
 function App() {
   return (
@@ -9,10 +11,17 @@ function App() {
       <h2>Gradebook</h2>
       <BrowserRouter>
           <div>
+          <nav>
+            <Link to="/">Assignment</Link> |
+            <Link to="/admin">Admin</Link> |
+            <Link to="/login">Login</Link>  
+          </nav>
             <Switch>
-              <Route exact path="/" component={ListAssignment} />
-              <Route path="/gradeAssignment" component={GradeAssignment} />
-              <Route render={ () => <h1>Page not found</h1>} />
+            <Route exact path="/" component={ListAssignment} />
+            <Route path="/schedule" component={GradeAssignment} />
+            <Route path="/admin" component={AddAssignment} />
+            <Route path="/login" component={Login} /> 
+            <Route render={() => <h1>Page not found</h1>} />
             </Switch>
           </div>
         </BrowserRouter>
--- src/components/GradeAssignment.js
+++ src/components/GradeAssignment.js
@@ -1,3 +1,4 @@
+GradeAssignment.js
 import React, {useState, useEffect}  from 'react';
 import {SERVER_URL} from '../constants';
 
